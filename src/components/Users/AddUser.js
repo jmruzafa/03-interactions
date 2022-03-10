7b5657ba17cd983b850856e66d04448a517395d3
Javier Reyes <javier.reyes@adidas.com>
2022-03-10 22:47:51 +0100

    Validation - 1st iteration

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
-    
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHanlder = (event) => {
+        //preventing default action of submitting the form
         event.preventDefault();
+
+        //validations 1st iteration
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+        //adding this "+" in JS we are sure it will be treated as a number
+        if (+enteredAge < 1){
+            return;
+        }
+        console.log(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
-    
+
+        
+    const usernameChangeHandler = (event) =>{
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) =>{
+        setEnteredAge(event.target.value);
+    };
+
     return (
-        <Card>
+        <Card className={classes.input}>
         <form onSubmit={addUserHanlder}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number" />
-            <button type="submit">Add User</button>
+            <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     )
