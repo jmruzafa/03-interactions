f6020313509384fcce378a2d7df18b335ba98c5d
Javier Reyes <javier.reyes@adidas.com>
2022-03-13 18:25:45 +0100

    Minimal functionality added. Happy path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [usersList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      //add new user to existing list 
+      //spread operator
+      return [...prevUserList, {id: Math.random().toString(), name: uName, age: uAge }];
+    });
+
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList}/>
     </div>
   );
 }
